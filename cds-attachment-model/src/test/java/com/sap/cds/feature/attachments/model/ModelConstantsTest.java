70080233e666666507fcbd37e709a4447e54c720
D043928 <andre.adam@sap.com>
2024-02-21 09:48:34 +0100

    correct and add test

--- cds-attachment-model/src/test/java/com/sap/cds/feature/attachments/model/ModelConstantsTest.java
+++ cds-attachment-model/src/test/java/com/sap/cds/feature/attachments/model/ModelConstantsTest.java
@@ -1,14 +1,19 @@
 package com.sap.cds.feature.attachments.model;
 
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
 
 class ModelConstantsTest {
 
     @Test
     void correctValueForMediaDataAnnotation() {
-        assertThat(ModelConstants.ANNOTATION_IS_MEDIA_DATA).isEqualTo("IsMediaData");
+        assertThat(ModelConstants.ANNOTATION_IS_MEDIA_DATA).isEqualTo("_is_media_data");
+    }
+
+    @Test
+    void correctValueForDocumentIdAnnotation() {
+        assertThat(ModelConstants.ANNOTATION_IS_EXTERNAL_DOCUMENT_ID).isEqualTo("_is_document_id");
     }
 
 }
