510dc5b92a6fb08603976551c7df4b423e754bc9
D043928 <andre.adam@sap.com>
2024-03-01 18:11:51 +0100

    remove not needed files

--- cds-attachment-service/src/main/java/com/sap/cds/feature/attachments/service/model/AttachmentModificationResult.java
+++
@@ -1,4 +0,0 @@
-package com.sap.cds.feature.attachments.service.model;
-
-public record AttachmentModificationResult(boolean isExternalStored, String documentId) {
-}
--- cds-extension-handler/src/main/resources/cds/com.sap.cds/cds-feature-attachments-handler/index.cds
+++
@@ -1,2 +0,0 @@
-using from`./attachments`;
-using from`./attachments-annotations`;
