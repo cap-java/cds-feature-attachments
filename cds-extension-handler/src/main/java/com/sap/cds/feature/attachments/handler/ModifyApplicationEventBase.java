21c56e558d2cbbb2044dfdcc723625e75b413f8d
D043928 <andre.adam@sap.com>
2024-03-01 15:59:47 +0100

    refactor

--- cds-extension-handler/src/main/java/com/sap/cds/feature/attachments/handler/ModifyApplicationEventBase.java
+++ cds-extension-handler/src/main/java/com/sap/cds/feature/attachments/handler/ModifyApplicationEventBase.java
@@ -18,42 +18,42 @@ import com.sap.cds.services.persistence.PersistenceService;
 
 abstract class ModifyApplicationEventBase extends ApplicationEventBase {
 
-		private static final Logger logger = LoggerFactory.getLogger(ModifyApplicationEventBase.class);
+	private static final Logger logger = LoggerFactory.getLogger(ModifyApplicationEventBase.class);
 
-		private final PersistenceService persistenceService;
-		private final ModifyAttachmentEventFactory eventFactory;
+	private final PersistenceService persistenceService;
+	private final ModifyAttachmentEventFactory eventFactory;
 
-		ModifyApplicationEventBase(PersistenceService persistenceService, ModifyAttachmentEventFactory eventFactory) {
-				this.persistenceService = persistenceService;
-				this.eventFactory = eventFactory;
+	ModifyApplicationEventBase(PersistenceService persistenceService, ModifyAttachmentEventFactory eventFactory) {
+		this.persistenceService = persistenceService;
+		this.eventFactory = eventFactory;
+	}
+
+	boolean processingNotNeeded(CdsEntity entity, List<CdsData> data) {
+		return !isContentFieldInData(entity, data);
+	}
+
+	void uploadAttachmentForEntity(CdsEntity entity, List<CdsData> data, String event) {
+		Filter filter = buildFilterForMediaTypeEntity();
+		Converter converter = (path, element, value) -> {
+			var fieldNames = getFieldNames(element, path.target());
+			var attachmentIdObject = path.target().keys().get(fieldNames.keyField());
+			var attachmentId = Objects.nonNull(attachmentIdObject) ? String.valueOf(attachmentIdObject) : null;
+			var oldData = CqnService.EVENT_UPDATE.equals(event) ? readExistingData(attachmentId, path.target().entity()) : CdsData.create();
+
+			var eventToProcess = eventFactory.getEvent(event, value, fieldNames, oldData);
+			return eventToProcess.processEvent(path, element, fieldNames, value, oldData, attachmentId);
+		};
+		callProcessor(entity, data, filter, converter);
+	}
+
+	private CdsData readExistingData(String attachmentId, CdsEntity entity) {
+		if (Objects.isNull(attachmentId)) {
+			logger.error("no id provided for attachment entity");
+			throw new IllegalStateException("no attachment id provided");
 		}
-
-		boolean processingNotNeeded(CdsEntity entity, List<CdsData> data) {
-				return !isContentFieldInData(entity, data);
-		}
-
-		void uploadAttachmentForEntity(CdsEntity entity, List<CdsData> data, String event) {
-				Filter filter = buildFilterForMediaTypeEntity();
-				Converter converter = (path, element, value) -> {
-						var fieldNames = getFieldNames(element, path.target());
-						var attachmentIdObject = path.target().keys().get(fieldNames.keyField());
-						var attachmentId = Objects.nonNull(attachmentIdObject) ? String.valueOf(attachmentIdObject) : null;
-						var oldData = CqnService.EVENT_UPDATE.equals(event) ? readExistingData(attachmentId, path.target().entity()) : CdsData.create();
-
-						var eventToProcess = eventFactory.getEvent(event, value, fieldNames, oldData);
-						return eventToProcess.processEvent(path, element, fieldNames, value, oldData, attachmentId);
-				};
-				callProcessor(entity, data, filter, converter);
-		}
-
-		private CdsData readExistingData(String attachmentId, CdsEntity entity) {
-				if (Objects.isNull(attachmentId)) {
-						logger.error("no id provided for attachment entity");
-						throw new IllegalStateException("no attachment id provided");
-				}
-				CqnSelect select = Select.from(entity).byId(attachmentId);
-				var result = persistenceService.run(select);
-				return result.single();
-		}
+		CqnSelect select = Select.from(entity).byId(attachmentId);
+		var result = persistenceService.run(select);
+		return result.single();
+	}
 
 }
