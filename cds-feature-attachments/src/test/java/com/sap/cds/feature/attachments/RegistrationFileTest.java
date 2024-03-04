d808d445377624f669fadd0e3f4a958a5e14942c
D043928 <andre.adam@sap.com>
2024-03-04 10:43:12 +0100

    add registration

---
+++ cds-feature-attachments/src/test/java/com/sap/cds/feature/attachments/RegistrationFileTest.java
@@ -0,0 +1,12 @@
+package com.sap.cds.feature.attachments;
+
+import org.junit.jupiter.api.Test;
+
+class RegistrationFileTest {
+
+	@Test
+	void registrationFileAvailable() {
+//		fail("not implemented");
+	}
+
+}
