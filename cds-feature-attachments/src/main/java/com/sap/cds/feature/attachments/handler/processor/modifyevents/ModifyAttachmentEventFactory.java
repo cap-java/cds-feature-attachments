695a57bb2da204391acedba0b6692db44685c501
D043928 <andre.adam@sap.com>
2024-03-19 10:23:01 +0100

    remove not needed dependency

--- cds-feature-attachments/src/main/java/com/sap/cds/feature/attachments/handler/processor/modifyevents/ModifyAttachmentEventFactory.java
+++ cds-feature-attachments/src/main/java/com/sap/cds/feature/attachments/handler/processor/modifyevents/ModifyAttachmentEventFactory.java
@@ -1,11 +1,9 @@
 package com.sap.cds.feature.attachments.handler.processor.modifyevents;
 
-import org.springframework.lang.Nullable;
-
 import com.sap.cds.CdsData;
 
 public interface ModifyAttachmentEventFactory {
 
-	ModifyAttachmentEvent getEvent(String event, @Nullable Object value, CdsData existingData);
+	ModifyAttachmentEvent getEvent(String event, Object value, CdsData existingData);
 
 }
