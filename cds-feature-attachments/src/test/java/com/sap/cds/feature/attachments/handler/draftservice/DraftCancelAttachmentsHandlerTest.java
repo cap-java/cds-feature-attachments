55a018d07e7b25b5444f74a679dbfc990bce4171
D043928 <andre.adam@sap.com>
2024-03-27 20:02:21 +0100

    support draft

---
+++ cds-feature-attachments/src/test/java/com/sap/cds/feature/attachments/handler/draftservice/DraftCancelAttachmentsHandlerTest.java
@@ -0,0 +1,22 @@
+package com.sap.cds.feature.attachments.handler.draftservice;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DraftCancelAttachmentsHandlerTest {
+
+	private DraftCancelAttachmentsHandler cut;
+
+	@BeforeEach
+	void setup() {
+		cut = new DraftCancelAttachmentsHandler();
+	}
+
+	@Test
+	void testProcessBeforeDraftCancel() {
+		fail("logic and test not implemented yet");
+	}
+
+}
