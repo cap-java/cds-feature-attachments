a0a35fdea63b983531d967cf33361f09b6d676af
D043928 <andre.adam@sap.com>
2024-04-18 14:03:19 +0200

    add test

---
+++ cds-feature-attachments/src/test/java/com/sap/cds/feature/attachments/handler/draftservice/DraftActiveAttachmentsHandlerTest.java
@@ -0,0 +1,39 @@
+package com.sap.cds.feature.attachments.handler.draftservice;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import com.sap.cds.feature.attachments.handler.applicationservice.helper.ThreadDataStorageSetter;
+import com.sap.cds.services.draft.DraftSaveEventContext;
+
+class DraftActiveAttachmentsHandlerTest {
+
+	private DraftActiveAttachmentsHandler cut;
+	private ThreadDataStorageSetter threadLocalSetter;
+	private ArgumentCaptor<Runnable> runnableCaptor;
+
+	@BeforeEach
+	void setup() {
+		threadLocalSetter = mock(ThreadDataStorageSetter.class);
+		cut = new DraftActiveAttachmentsHandler(threadLocalSetter);
+
+		runnableCaptor = ArgumentCaptor.forClass(Runnable.class);
+	}
+
+	@Test
+	void setterCalled() {
+		var context = mock(DraftSaveEventContext.class);
+
+		cut.processDraftSave(context);
+
+		verify(threadLocalSetter).set(eq(true), runnableCaptor.capture());
+		verifyNoInteractions(context);
+		runnableCaptor.getValue().run();
+		verify(context).proceed();
+	}
+
+}
