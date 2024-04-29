2b00c52ac9c23f567c9d7aaa9cefbce2a7370591
D043928 <andre.adam@sap.com>
2024-04-29 11:35:39 +0200

    test license script

--- cds-feature-attachments/src/main/java/com/sap/cds/feature/attachments/configuration/Registration.java
+++ cds-feature-attachments/src/main/java/com/sap/cds/feature/attachments/configuration/Registration.java
@@ -1,6 +1,3 @@
-/**************************************************************************
-	* (C) 2019-2024 SAP SE or an SAP affiliate company. All rights reserved. *
-	**************************************************************************/
 package com.sap.cds.feature.attachments.configuration;
 
 import java.time.Duration;
