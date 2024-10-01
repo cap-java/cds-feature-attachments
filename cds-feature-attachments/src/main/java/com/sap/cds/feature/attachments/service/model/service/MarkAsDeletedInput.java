8243a9aa1b3509bd95ded99c5a3ed82f44439917
André Adam <78218751+adamandre79@users.noreply.github.com>
2024-10-01 18:00:44 +0200

    Update cds-feature-attachments/src/main/java/com/sap/cds/feature/attachments/service/model/service/MarkAsDeletedInput.java
    Co-authored-by: Markus Ofterdinger <markus.ofterdinger@sap.com>

--- cds-feature-attachments/src/main/java/com/sap/cds/feature/attachments/service/model/service/MarkAsDeletedInput.java
+++ cds-feature-attachments/src/main/java/com/sap/cds/feature/attachments/service/model/service/MarkAsDeletedInput.java
@@ -3,7 +3,7 @@ package com.sap.cds.feature.attachments.service.model.service;
 import com.sap.cds.services.request.UserInfo;
 
 /**
-	* The class {@link MarkAsDeletedInput} is used to store the input for mark an attachment as deleted.
-	*/
+ * The class {@link MarkAsDeletedInput} is used to store the input for mark an attachment as deleted.
+ */
 public record MarkAsDeletedInput(String contentId, UserInfo userInfo) {
 }
