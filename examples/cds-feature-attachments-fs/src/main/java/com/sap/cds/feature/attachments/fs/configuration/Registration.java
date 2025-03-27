cb6550cad777c3e32be453269a2b42b388f7e1d0
Markus Ofterdinger <markus@ofterdinger.de>
2025-03-27 11:38:08 +0100

    added new event handler

--- examples/cds-feature-attachments-fs/src/main/java/com/sap/cds/feature/attachments/fs/configuration/Registration.java
+++ examples/cds-feature-attachments-fs/src/main/java/com/sap/cds/feature/attachments/fs/configuration/Registration.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import org.apache.commons.io.FileUtils;
 
 import com.sap.cds.feature.attachments.fs.handler.FSAttachmentsServiceHandler;
+import com.sap.cds.feature.attachments.fs.handler.FSDraftCreateHandler;
 import com.sap.cds.services.runtime.CdsRuntimeConfiguration;
 import com.sap.cds.services.runtime.CdsRuntimeConfigurer;
 
@@ -21,6 +22,7 @@ public class Registration implements CdsRuntimeConfiguration {
 
 		try {
 			configurer.eventHandler(new FSAttachmentsServiceHandler(rootFolder));
+			configurer.eventHandler(new FSDraftCreateHandler());
 		} catch (IOException e) {
 			throw new IllegalStateException("Error while creating the FSAttachmentsServiceHandler", e);
 		}
---
+++ examples/cds-feature-attachments-fs/src/main/java/com/sap/cds/feature/attachments/fs/handler/FSDraftCreateHandler.java
@@ -0,0 +1,68 @@
+/**************************************************************************
+ * (C) 2019-2024 SAP SE or an SAP affiliate company. All rights reserved. *
+ **************************************************************************/
+package com.sap.cds.feature.attachments.fs.handler;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sap.cds.CdsData;
+import com.sap.cds.feature.attachments.generated.cds4j.sap.attachments.Attachments;
+import com.sap.cds.feature.attachments.handler.common.ApplicationHandlerHelper;
+import com.sap.cds.reflect.CdsAssociationType;
+import com.sap.cds.reflect.CdsElement;
+import com.sap.cds.reflect.CdsEntity;
+import com.sap.cds.services.draft.DraftCreateEventContext;
+import com.sap.cds.services.draft.DraftService;
+import com.sap.cds.services.handler.EventHandler;
+import com.sap.cds.services.handler.annotations.Before;
+import com.sap.cds.services.handler.annotations.HandlerOrder;
+import com.sap.cds.services.handler.annotations.ServiceName;
+
+/**
+ * Event handler for draft creation events on the DraftService.
+ */
+@ServiceName(value = "*", type = DraftService.class)
+public class FSDraftCreateHandler implements EventHandler {
+
+	private static final Logger logger = LoggerFactory.getLogger(FSDraftCreateHandler.class);
+
+	@Before
+	@HandlerOrder(HandlerOrder.LATE)
+	void onCreateDraftAttachment(DraftCreateEventContext context, CdsData data) {
+		CdsEntity target = context.getTarget();
+
+		// check if target entity contains aspect Attachments
+		if (ApplicationHandlerHelper.isMediaEntity(target)) {
+			String fileName = (String) data.get(Attachments.FILE_NAME);
+			// get unique identifier of attachment's parent entity, e.g. the Books entity
+			Parent parent = getParentId(target, data);
+
+			logger.info("Creating draft attachment '{}' for parent entity '{}' with ID = {}", fileName, parent.entity,
+					parent.id);
+
+			// do something with the data of the draft attachments entity
+		}
+	}
+
+	private static Parent getParentId(CdsEntity target, CdsData data) {
+		// find association to parent entity
+		Optional<CdsElement> upAssociation = target.findAssociation("up_");
+
+		// if association is found, try to get foreign key to parent entity
+		if (upAssociation.isPresent()) {
+			// get association type
+			CdsAssociationType assocType = upAssociation.get().getType();
+			// get the refs of the association
+			List<String> fkElements = assocType.refs().map(ref -> "up__" + ref.path()).toList();
+			return new Parent(assocType.getTarget(), data.get(fkElements.get(0)));
+		}
+		return null;
+	}
+
+	record Parent(CdsEntity entity, Object id) {
+	}
+}
