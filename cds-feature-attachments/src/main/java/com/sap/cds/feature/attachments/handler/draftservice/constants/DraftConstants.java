2d30c4a53068cb010a90d9a43b5fba51f986bae7
Markus Ofterdinger <markus.ofterdinger@sap.com>
2025-04-09 08:27:35 +0200

    Use Drafts.SIBLING_ENTITY (#423)

--- cds-feature-attachments/src/main/java/com/sap/cds/feature/attachments/handler/draftservice/constants/DraftConstants.java
+++ cds-feature-attachments/src/main/java/com/sap/cds/feature/attachments/handler/draftservice/constants/DraftConstants.java
@@ -6,7 +6,6 @@ package com.sap.cds.feature.attachments.handler.draftservice.constants;
 
 public final class DraftConstants {
 
-	public static final String SIBLING_ENTITY = "SiblingEntity";
 	public static final String DRAFT_TABLE_POSTFIX = "_drafts";
 
 	private DraftConstants() {
