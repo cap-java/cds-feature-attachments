e669ce0691dc90feffc269078bdaead2435d845a
Markus Ofterdinger <markus@ofterdinger.de>
2025-07-28 16:15:11 +0200

    simplified test handler

--- integration-tests/srv/src/main/java/com/sap/cds/feature/attachments/integrationtests/testhandler/TestPluginAttachmentsServiceHandler.java
+++ integration-tests/srv/src/main/java/com/sap/cds/feature/attachments/integrationtests/testhandler/TestPluginAttachmentsServiceHandler.java
@@ -11,8 +11,6 @@ import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.Marker;
-import org.slf4j.MarkerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -32,15 +30,14 @@ import com.sap.cds.services.handler.annotations.ServiceName;
 @Component
 public class TestPluginAttachmentsServiceHandler implements EventHandler {
 
-	private static final Marker marker = MarkerFactory.getMarker("DUMMY_HANDLER");
 	private static final Logger logger = LoggerFactory.getLogger(TestPluginAttachmentsServiceHandler.class);
 
 	private static final Map<String, byte[]> documents = new HashMap<>();
 	private static final List<EventContextHolder> eventContextHolder = new ArrayList<>();
 
-	@On(event = AttachmentService.EVENT_CREATE_ATTACHMENT)
-	public void createAttachment(AttachmentCreateEventContext context) throws IOException {
-		logger.info(marker, "CREATE Attachment called in dummy handler");
+	@On
+	void createAttachment(AttachmentCreateEventContext context) throws IOException {
+		logger.info("CREATE Attachment called in dummy handler");
 		var contentId = UUID.randomUUID().toString();
 		documents.put(contentId, context.getData().getContent().readAllBytes());
 		context.setContentId(contentId);
@@ -49,16 +46,16 @@ public class TestPluginAttachmentsServiceHandler implements EventHandler {
 		eventContextHolder.add(new EventContextHolder(AttachmentService.EVENT_CREATE_ATTACHMENT, context));
 	}
 
-	@On(event = AttachmentService.EVENT_MARK_ATTACHMENT_AS_DELETED)
-	public void markAttachmentAsDeleted(AttachmentMarkAsDeletedEventContext context) {
-		logger.info(marker, "DELETE Attachment called in dummy handler for document id {}", context.getContentId());
+	@On
+	void markAttachmentAsDeleted(AttachmentMarkAsDeletedEventContext context) {
+		logger.info("DELETE Attachment called in dummy handler for document id {}", context.getContentId());
 		context.setCompleted();
 		eventContextHolder.add(new EventContextHolder(AttachmentService.EVENT_MARK_ATTACHMENT_AS_DELETED, context));
 	}
 
-	@On(event = AttachmentService.EVENT_READ_ATTACHMENT)
-	public void readAttachment(AttachmentReadEventContext context) {
-		logger.info(marker, "READ Attachment called in dummy handler for content id {}", context.getContentId());
+	@On
+	void readAttachment(AttachmentReadEventContext context) {
+		logger.info("READ Attachment called in dummy handler for content id {}", context.getContentId());
 		var content = documents.get(context.getContentId());
 		var stream = Objects.nonNull(content) ? new ByteArrayInputStream(content) : null;
 		context.getData().setContent(stream);
@@ -66,9 +63,9 @@ public class TestPluginAttachmentsServiceHandler implements EventHandler {
 		eventContextHolder.add(new EventContextHolder(AttachmentService.EVENT_READ_ATTACHMENT, context));
 	}
 
-	@On(event = AttachmentService.EVENT_RESTORE_ATTACHMENT)
-	public void restoreAttachment(AttachmentRestoreEventContext context) {
-		logger.info(marker, "RESTORE Attachment called in dummy handler for timestamp {}", context.getRestoreTimestamp());
+	@On
+	void restoreAttachment(AttachmentRestoreEventContext context) {
+		logger.info("RESTORE Attachment called in dummy handler for timestamp {}", context.getRestoreTimestamp());
 		context.setCompleted();
 		eventContextHolder.add(new EventContextHolder(AttachmentService.EVENT_RESTORE_ATTACHMENT, context));
 	}
@@ -78,7 +75,8 @@ public class TestPluginAttachmentsServiceHandler implements EventHandler {
 		if (event.equals(AttachmentService.EVENT_CREATE_ATTACHMENT) && !context.isEmpty()) {
 			context.forEach(c -> {
 				var createContext = (AttachmentCreateEventContext) c.context();
-				createContext.getData().setContent(new ByteArrayInputStream(documents.get(createContext.getContentId())));
+				createContext.getData()
+						.setContent(new ByteArrayInputStream(documents.get(createContext.getContentId())));
 			});
 		}
 		return context;
