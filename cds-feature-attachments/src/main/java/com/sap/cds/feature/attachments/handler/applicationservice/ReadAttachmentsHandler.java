50aac2d43bed73290f6ad518842c63a8f3378460
Lisa Julia Nebel <lisajuliafog@yahoo.de>
2025-09-02 15:43:36 +0200

    Fix usage of malware scanner with non-HANA storage

--- cds-feature-attachments/src/main/java/com/sap/cds/feature/attachments/handler/applicationservice/ReadAttachmentsHandler.java
+++ cds-feature-attachments/src/main/java/com/sap/cds/feature/attachments/handler/applicationservice/ReadAttachmentsHandler.java
@@ -95,11 +95,10 @@ public class ReadAttachmentsHandler implements EventHandler {
           (path, element, value) -> {
             Attachments attachment = Attachments.of(path.target().values());
             InputStream content = attachment.getContent();
-            boolean contentExists = nonNull(content);
-            if (nonNull(attachment.getContentId()) || contentExists) {
-              verifyStatus(path, attachment, contentExists);
+            if (nonNull(attachment.getContentId())) {
+              verifyStatus(path, attachment);
               Supplier<InputStream> supplier =
-                  contentExists
+                  nonNull(content)
                       ? () -> content
                       : () -> attachmentService.readAttachment(attachment.getContentId());
               return new LazyProxyInputStream(supplier, statusValidator, attachment.getStatus());
@@ -147,15 +146,14 @@ public class ReadAttachmentsHandler implements EventHandler {
     return associationNames;
   }
 
-  private void verifyStatus(Path path, Attachments attachment, boolean contentExists) {
+  private void verifyStatus(Path path, Attachments attachment) {
     if (areKeysEmpty(path.target().keys())) {
       logger.debug(
           "In verify status for content id {} and status {}",
           attachment.getContentId(),
           attachment.getStatus());
-      if ((StatusCode.UNSCANNED.equals(attachment.getStatus())
-              || StatusCode.SCANNING.equals(attachment.getStatus()))
-          && contentExists) {
+      if (StatusCode.UNSCANNED.equals(attachment.getStatus())
+          || StatusCode.SCANNING.equals(attachment.getStatus())) {
         logger.debug(
             "Scanning content with ID {} for malware, has current status {}",
             attachment.getContentId(),
