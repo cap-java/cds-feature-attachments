fcb762680fcfa67a33668840037219bef581ebe4
Markus Ofterdinger <markus@ofterdinger.de>
2025-09-03 12:51:34 +0200

    The serializable class ObjectStoreServiceException does not declare a
    static final serialVersionUID field of type long

--- storage-targets/cds-feature-attachments-oss/src/main/java/com/sap/cds/feature/attachments/oss/handler/ObjectStoreServiceException.java
+++ storage-targets/cds-feature-attachments-oss/src/main/java/com/sap/cds/feature/attachments/oss/handler/ObjectStoreServiceException.java
@@ -4,6 +4,8 @@
 package com.sap.cds.feature.attachments.oss.handler;
 
 public class ObjectStoreServiceException extends RuntimeException {
+  private static final long serialVersionUID = -360151881669215771L;
+
   public ObjectStoreServiceException(String message) {
     super(message);
   }
