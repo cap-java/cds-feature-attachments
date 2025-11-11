6a541e81846c9aa7fca3c9c608f17835b6210f76
Lisa Julia Nebel <lisa.nebel@sap.com>
2025-11-11 11:31:55 +0100

    Add 'isAttachmentEntity' and 'hasAttachmentAssociations' checks to DraftCancelAttachmentsHandler

--- cds-feature-attachments/src/main/java/com/sap/cds/feature/attachments/handler/draftservice/DraftCancelAttachmentsHandler.java
+++ cds-feature-attachments/src/main/java/com/sap/cds/feature/attachments/handler/draftservice/DraftCancelAttachmentsHandler.java
@@ -58,7 +58,10 @@ public class DraftCancelAttachmentsHandler implements EventHandler {
   @Before
   @HandlerOrder(HandlerOrder.LATE)
   void processBeforeDraftCancel(DraftCancelEventContext context) {
-    if (isWhereEmpty(context)) {
+    // We only process the draft cancel event if there is no WHERE clause in the CQN
+    // and if the target entity is an attachment entity or has attachment associations.
+    if ((isAttachmentEntity(context.getTarget()) || hasAttachmentAssociations(context.getTarget()))
+        && isWhereEmpty(context)) {
       logger.debug(
           "Processing before {} event for entity {}", context.getEvent(), context.getTarget());
 
@@ -104,7 +107,21 @@ public class DraftCancelAttachmentsHandler implements EventHandler {
   // https://github.com/cap-java/cds-feature-attachments/blob/main/doc/Design.md#events
   // Unfortunately, context.getEvent() does not return a reliable value in this case.
   private boolean isWhereEmpty(DraftCancelEventContext context) {
-    return context.getCqn().where().isEmpty();
+    System.out.println(context.getCqn().where());
+    return true; // context.getCqn().where().isEmpty();
+  }
+
+  // This function checks if the given entity is of type Attachments
+  private boolean isAttachmentEntity(CdsEntity entity) {
+    return entity.getQualifiedName().toLowerCase().contains("attachment")
+        || entity.elements().anyMatch(element -> element.getName().equals(Attachments.FILE_NAME));
+  }
+
+  // This function checks if the given entity has attachment associations.
+  private boolean hasAttachmentAssociations(CdsEntity entity) {
+    return entity
+        .elements()
+        .anyMatch(element -> element.getName().toLowerCase().contains("attachment"));
   }
 
   private List<Attachments> readAttachments(
