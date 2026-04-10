f7bd3f504654d9ccb5a5c95989109d0124eb3c17
Marvin Lindner <marvin.lindner@sap.com>
2026-04-10 17:40:57 +0200

    remove unused import

--- storage-targets/cds-feature-attachments-oss/src/test/java/com/sap/cds/feature/attachments/oss/client/AWSClientTest.java
+++ storage-targets/cds-feature-attachments-oss/src/test/java/com/sap/cds/feature/attachments/oss/client/AWSClientTest.java
@@ -4,7 +4,6 @@
 package com.sap.cds.feature.attachments.oss.client;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
